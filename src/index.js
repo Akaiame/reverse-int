43d4e73a3bc7ce3cb5f6a31b8c70519e42e78cf8
Dmitry Ilichov <dmitryilichov@gmail.com>
2023-08-24 16:22:20 +0300

    completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+
+    if (n < 0) n *= -1;
+
+    let arrayOfSingleDigits = n.toString(10).split("").map(function(element) {
+        return parseInt(element, 10);
+    });
+    let stringNumber = [];
+
+    for (let i = arrayOfSingleDigits.length - 1; i >= 0; i--) {
+        stringNumber += arrayOfSingleDigits[i];
+    }
   
+    let result = Number(stringNumber);
+    return result;
 }
+
